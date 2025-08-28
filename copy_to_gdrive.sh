ade7284fc912963ac91cd6cb822e3c2e5089c612
Illyas Mansoor <mimansoor@gmail.com>
2025-08-28 10:34:16 +0530

    using direct gdrive path instead of mount path

--- copy_to_gdrive.sh
+++ copy_to_gdrive.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 SRC="/home/immansoo/options_zero_game/experiments/options_zero_game_muzero_agent_v2.2-Final_ns50_upc2000_bs256/ckpt/ckpt_best.pth.tar"
-DEST="/home/immansoo/gdrive/options_zero_game"
+DEST="gdrive:/options_zero_game/"
 
 while true
 do
-    echo "$(date '+%Y-%m-%d %H:%M:%S') - Syncing..."
-    rsync -u "$SRC" "$DEST"
+    echo "$(date '+%Y-%m-%d %H:%M:%S') - Uploading to Google Drive..."
+    rclone copy "$SRC" "$DEST" --progress
 
     echo "Waiting 1 hour..."
     sleep 3600
